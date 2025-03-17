067b4be4a29494ee9f3e82072c218d21010e797c
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2025-03-17 16:58:32 -0400

    Added ZodFormField and zod schema helpers

---
+++ src/components/forms/ZodFormField.tsx
@@ -0,0 +1,44 @@
+import { ComponentProps } from "react"
+import { Path, UseFormReturn } from "react-hook-form"
+import { TypeOf, z } from "zod"
+import { FormItem, FormLabel, FormControl, FormMessage, FormField } from "../ui/form.js"
+import { Input } from "../ui/input.js"
+import { ZodFormFieldMetaData } from "./zod_schema_helpers.js"
+
+type ZodFormField<TSchema extends z.ZodObject<any>> = {
+    form: UseFormReturn<any, any, undefined>;
+    name: Path<TypeOf<TSchema>>;
+    fieldMetaData: ZodFormFieldMetaData<string> | ZodFormFieldMetaData<boolean> | ZodFormFieldMetaData<Record<string, any>> | ZodFormFieldMetaData<string | Record<string, any>> | ZodFormFieldMetaData<File[] | undefined> | ZodFormFieldMetaData<object> | ZodFormFieldMetaData<any>
+}
+
+const fieldComponents: Record<string, any> = {
+    string: Input,
+};
+
+
+export default function <TSchema extends z.ZodObject<any>>({ form, fieldMetaData, name }: ZodFormField<TSchema>) {
+    let Component = fieldMetaData.component?.component ?? fieldComponents["string"]; // Default to Input
+
+    return (
+        <FormField
+            control={form.control}
+            name={name}
+            render={({ field }) => {
+                const label = fieldMetaData.label ?? name
+                const additionalProps = (typeof fieldMetaData.component?.props === 'function') ? fieldMetaData.component.props(field, form) : {}
+                const fieldProps: ComponentProps<typeof Component> = {...fieldMetaData.field, ...additionalProps}
+
+                if(fieldProps.type === "hidden")
+                    return <><Component {...form.register(name)} {...fieldProps} /><FormMessage /></>
+                    
+                return <FormItem>
+                    {label && <FormLabel>{label}</FormLabel>}
+                    <FormControl>
+                        <Component {...form.register(name)} {...fieldProps} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            }}
+        />
+    )
+}
---
+++ src/components/forms/zod_schema_helpers.ts
@@ -0,0 +1,47 @@
+import { ComponentProps, ComponentType } from "react";
+import { UseFormReturn } from "react-hook-form";
+import { z, ZodType } from "zod";
+
+export type ZodFormFieldMetaData<T> = {
+    defaultValue?: T | ((data?: any, allData?: any, name?: any) => T),
+    label?: string,
+    field?: Partial<HTMLInputElement>,
+    component?: {
+        component: ComponentType<any>,
+        props?: (field: ComponentProps<ComponentType>, form?: UseFormReturn<any, any, undefined>) => ComponentProps<ComponentType>
+    }
+}
+
+export type ZodFormMetadata<T> = {
+    [key in keyof T]?: ZodFormFieldMetaData<T[key]>;
+} & Record<string, any>; // <-- Allow additional dynamic fields
+
+// Generic function that accepts any Zod schema and generates metadata with the same keys
+export function createZodFormMetadata<T extends ZodType<any>>(_schema: T): ZodFormMetadata<z.infer< T>> {
+    type SchemaType = z.infer<T>;
+
+    const metadata: {
+        [key in keyof SchemaType]?: ZodFormFieldMetaData<SchemaType[key]>;
+    } = {} as any;
+
+    return metadata;
+}
+
+export const getDefaultValuesFromZodFormMetadata = (formMetaData: ReturnType<typeof createZodFormMetadata>, data?: Record<string, any>) => {
+    let defaultValues: Record<string, any> = {}; // Ensure it's an object with string keys
+
+    for (let key in formMetaData) {
+        const fieldKey = key // as keyof typeof notesCreateFormMetadata
+
+        if (formMetaData[fieldKey]?.defaultValue !== undefined) {
+            if (typeof formMetaData[fieldKey]?.defaultValue === 'function') {
+                defaultValues[fieldKey] = formMetaData[fieldKey]?.defaultValue(data?.[fieldKey], data, fieldKey)
+            } else {
+                defaultValues[fieldKey] = data?.[fieldKey] ?? formMetaData[fieldKey]?.defaultValue;
+            }
+        }
+    }
+
+    return defaultValues;
+};
+
