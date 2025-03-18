f14fc327ab04e23d6911a45529ee70df310af584
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2025-03-18 12:26:13 -0400

    Added standalone mode to message

--- src/components/ui/Message.tsx
+++ src/components/ui/Message.tsx
@@ -3,9 +3,10 @@ import { Card, CardContent } from "./card.js"
 type Message = {
     className?: string,
     type:  "error" | "warning" | "info" | "success" | "message",
-    children: React.ReactNode
+    children: React.ReactNode,
+    standalone?: boolean,
 }
-export function Message ({type, children, className}: Message) {
+export function Message ({type, children, className, standalone}: Message) {
     const typeStyles = {
         message: "message bg-message text-message",
         info: "info bg-info text-info",
@@ -17,7 +18,7 @@ export function Message ({type, children, className}: Message) {
     const style = typeStyles[type ?? "message"]; // Fallback to default
 
     return (
-        <Card className={`m-1 ${style} ${className??""}`}>
+        <Card className={`m-1 ${standalone && "w-96 mx-auto my-24"} ${style} ${className??""}`}>
             <CardContent>{children}</CardContent>
         </Card>
     );
