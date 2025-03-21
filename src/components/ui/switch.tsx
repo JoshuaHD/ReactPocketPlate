46bc0887f06037f1f6fcda1ea0c8d38f96e038ed
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2025-03-21 15:58:03 -0400

    added big switch button variation

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -3,10 +3,19 @@ import * as SwitchPrimitive from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils.js"
 
+type Switch = {
+  size: "large"|undefined
+}
+
 function Switch({
   className,
   ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+}: React.ComponentProps<typeof SwitchPrimitive.Root> & Switch) {
+  const size = props.size
+
+  if(size==="large")
+    return <SwitchBig className={className} {...props} />
+    
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
@@ -25,5 +34,29 @@ function Switch({
     </SwitchPrimitive.Root>
   )
 }
+
+  function SwitchBig({
+    className,
+    ...props
+  }: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+    return (
+      <SwitchPrimitive.Root
+        data-slot="switch"
+        className={cn(
+          "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-9 w-16 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50", // Switch size doubled
+          className
+        )}
+        {...props}
+      >
+        <SwitchPrimitive.Thumb
+          data-slot="switch-thumb"
+          className={cn(
+            "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block h-8 w-8 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-0.2rem)] data-[state=unchecked]:translate-x-[0.08rem]" // Thumb size increased and fixed translation
+          )}
+        />
+      </SwitchPrimitive.Root>
+    )
+  }
+  
 
 export { Switch }
