a9f91fff4a74388c6045250cd2bf9d05805b79d7
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2025-03-24 11:59:32 -0400

    Added types and a throw an error if an array is provided when a single string value is expected.

--- src/components/forms/custom_components/SelectComponent.tsx
+++ src/components/forms/custom_components/SelectComponent.tsx
@@ -7,30 +7,36 @@ import { z } from "zod"
 type SelectOption = {
     value: string
     label: string | number | React.ReactNode
+    className?: string
 }
 
 type SelectFormField = {
     options: SelectOption[],
-
+    className?: string,
+    onChange: () => void
     field?: {
         options?: SelectOption[],
+        className?: string,
     }
 }
 
-export function SelectFormField (props: SelectFormField & ComponentProps<typeof Select>) {
+export function SelectFormField(props: SelectFormField & ComponentProps<typeof Select>) {
+    if (Array.isArray(props.value))
+        throw new Error("Value for Select FormField needs to be a string, got an Array")
 
-    return <Select 
-        onValueChange={props.onChange as typeof props.onValueChange} 
-        defaultValue={props.value}>
-        <FormControl>
+    return <Select
+        onValueChange={props.onChange as typeof props.onValueChange}
+        defaultValue={props.value}
+        >
+        <FormControl className={props.className ?? "w-full"}>
             <SelectTrigger>
                 <SelectValue placeholder="Please select an option" />
             </SelectTrigger>
         </FormControl>
         <SelectContent>
             {props.options.map((option: SelectOption, i: number) => {
-            return <SelectItem key={i} value={option.value}>{option.label}</SelectItem>
-        })}
+                return <SelectItem key={i} value={option.value} className={option.className}>{option.label}</SelectItem>
+            })}
         </SelectContent>
     </Select>
 }
